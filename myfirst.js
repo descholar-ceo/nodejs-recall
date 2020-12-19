061b75557166de2b2ef5ec1547f4a29add80d42d
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-19 09:14:50 +0200

    Get a url

--- myfirst.js
+++ myfirst.js
@@ -3,7 +3,6 @@ var dateTime = require('./my_modules/date-module');
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(
-    'Hello there! Welcome to the new world of nodejs!!! \nCurrent time is: '+dateTime.myDateTime());
+  res.write(req.url);
   res.end();
 }).listen(8080);

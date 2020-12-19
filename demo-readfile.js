67601cc59c9e5b33a2040dd9a71320c089cef677
descholar-ceo <emmanuellamugi@gmail.com>
2020-12-19 11:07:14 +0200

    Create sample documents

--- demo-readfile.js
+++ demo-readfile.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+  fs.readFile('./helper-files/example-1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
